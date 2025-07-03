87ace9ca86d40b14947993087f67891ae494d3c7
laberttt <labert.ribeiroo@gmail.com>
2025-07-03 20:14:21 -0300

    verificando ganhador por linha e coluna

--- tabuleiro.py
+++ tabuleiro.py
@@ -12,4 +12,21 @@ class Tabuleiro:
        
         
     def tem_campeao(self):
+        #somando linhas
+        for linha in self.matriz:
+            soma = sum(linha)
+            if soma == 3:
+                return Tabuleiro.JOGADOR_0
+            elif soma == 12:
+                return Tabuleiro.JOGADOR_X
+            
+        #somando colunas
+        for coluna in range(0, 3):
+            soma = self.matriz[0][coluna] + self.matriz[1][coluna] + self.matriz[2][coluna]
+            if soma == 3:
+                return Tabuleiro.JOGADOR_0
+            elif soma == 12:
+                return Tabuleiro.JOGADOR_X
+            
+            
         return Tabuleiro.DESCONHECIDO
